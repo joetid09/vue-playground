f03a8ed86f2f185a82aeadbe7614218423f32110
joetid09 <joseph.tidwell09@gmail.com>
2024-10-07 21:12:04 -0500

    created reactive counter on app mount

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import{ref} from 'vue'
+const counter = ref({count: 0})
+</script>
+
+<template>
+  <div id="app">
+    <button @click="counter.count++">{{ counter.count}}</button>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
